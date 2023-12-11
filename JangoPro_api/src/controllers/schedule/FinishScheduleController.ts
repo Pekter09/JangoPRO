7dc805d68735fb3f828d6460da544438acee0112
Pedro Couto Campos <pedrocoutocampos@outlook.com>
2023-12-11 10:29:29 -0300

    Deploy

--- JangoPro_api/src/controllers/schedule/FinishScheduleController.ts
+++ JangoPro_api/src/controllers/schedule/FinishScheduleController.ts
@@ -1,22 +1,27 @@
-import { request, Request, response, Response } from "express";
+import { Request, Response } from "express";
 import prismaClient from "../../prisma";
 import { FinishScheduleService } from "../../services/schedule/FinishScheduleService";
 
-class FinishScheduleController{
-    async handle(request: Request, response: Response){
-        const user_id = request.user_id;
-        const schedule_id = request.query.schedule_id as string;
-
-        const finishScheduleService = new FinishScheduleService(prismaClient);
-
-        const schedule = await finishScheduleService.execute({
-            user_id,
-            schedule_id,
-        })
-
-        return response.json(schedule)
-
-    }
+declare module "express" {
+  interface Request {
+    user_id: string;
+  }
 }
 
-export { FinishScheduleController }
+class FinishScheduleController {
+  async handle(request: Request, response: Response) {
+    const user_id = request.user_id;
+    const schedule_id = request.query.schedule_id as string;
+
+    const finishScheduleService = new FinishScheduleService(prismaClient);
+
+    const schedule = await finishScheduleService.execute({
+      user_id,
+      schedule_id,
+    });
+
+    return response.json(schedule);
+  }
+}
+
+export { FinishScheduleController };
